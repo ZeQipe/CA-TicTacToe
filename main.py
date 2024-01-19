2db76b9e9830f79c4788e5969ec52a1daafb395a
zeqipe <zeqipe@vk.com>
2024-01-19 16:29:52 +0300

    Add annotation all function

--- main.py
+++ main.py
@@ -3,24 +3,48 @@ import random
 import os
 
 
-def print_board(board):
+def print_board(board: list) -> None:
+    """
+    Выводит на консоль игровое поле...
+    :param board: type str
+    :return: None
+    """
+    clear = lambda: os.system('cls')
+    clear()
     for i in range(0, 9, 3):
         print("|".join(board[i:i + 3]))
 
 
-def make_move(board, position, current_player):
+def make_move(board: list, position: int, current_player: str) -> bool:
+    """
+    Делает ход, меняя пустое значение на доске на нужный символ игрока.
+    Предварительно проверяет, пустая ли позиция для хода.
+    :param board: str
+    :param position: int
+    :param current_player: str
+    :return: Вернет True, если ход сделан, либо False, если ячейка не пуста.
+    """
     if board[position] == " ":
         board[position] = current_player
         return True
     return False
 
 
-def switch_player(current_player):
+def switch_player(current_player: str) -> str:
+    """
+    После успешного хода, меняет текущего пользователя.
+    :param current_player: str
+    :return: str ('X' or 'O').
+    """
     return "O" if current_player == "X" else "X"
 
 
-def check_winner(board):
-    # Check rows, columns, and diagonals for a win
+def check_winner(board: list) -> bool:
+    """
+    Проверяет на то, что один из игроков уже победил.
+    :param board: str
+    :return: Вернет True, если есть победная комбинация. Иначе вернёт False.
+    """
     for i in range(0, 9, 3):
         if board[i] == board[i + 1] == board[i + 2] != " ":
             return True
@@ -33,25 +57,30 @@ def check_winner(board):
     return False
 
 
-async def bot_move(board, current_player):
-    await asyncio.sleep(1)  # Simulating bot's "thinking" time
+async def bot_move(board: list) -> None:
+    """
+
+    :param board:
+    :return:
+    """
+    await asyncio.sleep(1)
     return random.choice([i for i, x in enumerate(board) if x == " "])
 
 
-async def play_game():
+async def play_game() -> None:
+    """
+    Контролирует весь игровой процесс.
+    :return:
+    """
     board = [" "] * 9
     current_player = "X"
-    clear = lambda: os.system('cls')
 
     while True:
-        clear()
         print_board(board)
-
-        move = await bot_move(board, current_player)
+        move = await bot_move(board)
 
         if make_move(board, move, current_player):
             if check_winner(board):
-                clear()
                 print_board(board)
                 input(f"{current_player} wins!")
                 break
@@ -59,5 +88,6 @@ async def play_game():
             current_player = switch_player(current_player)
 
 
+#Run app and game
 if __name__ == "__main__":
     asyncio.run(play_game())
